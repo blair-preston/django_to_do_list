0d6c9cebf5dd65670399d9d6847fe83780aac526
Blair Preston <blair.preston@student.awesomeinc.org>
2022-04-14 18:23:08 +0000

    change hyperlinks to regular model

--- todoapp/serializers.py
+++ todoapp/serializers.py
@@ -2,39 +2,39 @@ from .models import User, Category, Event, Daily, Priority, Status, Todo
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
    
     # need the above statement if using hyperlink, can just take hyper link off
     class Meta:
         model = Category
         fields = '__all__'
 
-class EventSerializer(serializers.HyperlinkedModelSerializer):
+class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
 
-class DailySerializer(serializers.HyperlinkedModelSerializer):
+class DailySerializer(serializers.ModelSerializer):
     class Meta:
         model = Daily
         fields = '__all__'
 
-class PrioritySerializer(serializers.HyperlinkedModelSerializer):
+class PrioritySerializer(serializers.ModelSerializer):
     class Meta:
         model = Priority
         fields = '__all__'
 
-class StatusSerializer(serializers.HyperlinkedModelSerializer):
+class StatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = Status
         fields = '__all__'
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'
