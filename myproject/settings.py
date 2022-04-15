55dafec49dc075e1b7f3e1f95b712190d7bdd897
Blair Preston <blair.preston@student.awesomeinc.org>
2022-04-15 13:56:19 +0000

    authorization tests

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os 
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +81,16 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
         
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
+
+SIMPLE_JWT = {
+'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 # Database
--- todoapp/tests_app.py
+++ todoapp/tests_app.py
@@ -1,23 +1,28 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User as Djuser
+from .models import User
 import json
 
-test_user = {"username": "testuser", "password": "testpassword"}
+test_djuser = {"username": "testuser", "password": "testpassword"}
 
 # class allowing users to create a username and password
 # and then to be allowed to log in
-class UsersTest(TestCase):
+class DjUsersTest(TestCase):
     def setUp(self):
+        new_djuser = Djuser.objects.create(
+            username= test_djuser["username"])
+        new_djuser.set_password(test_djuser["password"])
+        new_djuser.save()
         new_user = User.objects.create(
-            username=test_user["username"])
-        new_user.set_password(test_user["password"])
+            name= 'Dragon'
+        )
         new_user.save()
 
     def get_token(self):
         res = self.client.post('/api/token/',
             data=json.dumps({
-                'username': test_user["username"],
-                'password': test_user["password"],
+                'username': test_djuser["username"],
+                'password': test_djuser["password"],
             }),
         content_type='application/json',
         )
@@ -27,41 +32,47 @@ class UsersTest(TestCase):
 
     # test should return 401 error
     # Make sure that unauthorized users are not allowed to POST
-    def test_unauthorized_cant_add_dailies(self):
-        res = self.client.post('/api/dailies/',
-        data=json.dumps({
-            "label": "Read book club chapter",
-            "category_id": 9,
-            "user_id": 4
-        }),
-        content_type='application/json',
-        )
-        self.assertEquals(res.status_code, 400)
-        res = self.client.post('/api/dailies/',
-        data=json.dumps({
-        "label": "20 push-ups",
-        "category_id": 8,
-        "user_id": 3
-        }),
-        content_type='application/json',
-        HTTP_AUTHORIZATION=f'Bearer WRONG TOKEN'
-        )
-        self.assertEquals(res.status_code, 400)
+    def test_unauthorized_cant_add_category(self):
+        testuser = User.objects.last(),
+        testid = int(testuser[0].id)
+        res = self.client.post('/api/categories/',
+            data=json.dumps({
+                "title": "Personal",
+                "color": "Yellow",
+                "user_id": testid 
+            }),
+            content_type='application/json',
+            HTTP_AUTHORIZATION=f'Bearer WRONG TOKEN'
+            )
+        self.assertEquals(res.status_code, 401)
+        res = self.client.post('/api/categories/',
+                                data=json.dumps({
+                                    "title": "House",
+                                    "color": "Purple",
+                                    "user_id": testid 
+                                }),
+                                content_type='application/json',
+                                HTTP_AUTHORIZATION=f'Bearer WRONG TOKEN'
+                                )
+        self.assertEquals(res.status_code, 401)
+        
 
         # Authorized users are allowed to POST
-    def test_authorized_can_add_dailes(self):
+    def test_authorized_can_add_category(self):
+        testuser = User.objects.last(),
+        testid = int(testuser[0].id)
         token = self.get_token()
-        res = self.client.post('/api/dailies/',
+        res = self.client.post('/api/categories/',
                                 data=json.dumps({
-                                    "label": "Read book club chapter",
-                                    "category_id": 9,
-                                    "user_id": 4
+                                    "title": "House",
+                                    "color": "Purple",
+                                    "user_id": testid 
                                 }),
                                 content_type='application/json',
                                 HTTP_AUTHORIZATION=f'Bearer {token}'
                                 )
         self.assertEquals(res.status_code, 201)
-        result = json.loads(res.content)["data"]
-        self.assertEquals(result["label"], '20 push-ups')
-        self.assertEquals(result["category_id"], 8)
-        self.assertEquals(result["user_id"], 4)
+        result = json.loads(res.content)
+        self.assertEquals(result["title"], 'House')
+        self.assertEquals(result["color"], 'Purple')
+
